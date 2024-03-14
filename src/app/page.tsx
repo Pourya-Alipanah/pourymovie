b778b4910153fcf912e85199961cbbaf2485ae68
Pourya_Alipanah <pourya.alipanah2@gmail.com>
2024-03-14 15:28:44 +0330

    initialize-font-configs
    initialize font and metadata tags config for start the project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="grid h-screen place-content-center text-center">
+      در دست ساخت
+      <div lang="en">Under Construction</div>
+    </div>
+  );
+}
---
+++ src/constants/metaTagInfo.ts
@@ -0,0 +1,7 @@
+// for main page
+export const { title, description } = {
+  title: "Poury Movie",
+
+  description:
+    "Discover the magic of cinema at Poury Movie. Explore a curated collection of films spanning genres and eras. With meticulously crafted metadata, your cinematic journey starts here. Welcome to Poury Movie – where every click unlocks a world of movie magic.",
+};
